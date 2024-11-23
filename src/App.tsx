4f4e159cfc4f998a503595317a2e7568f1e948cc
T_Mori <morit0301@gmail.com>
2024-11-23 19:28:31 +0900

    BrowserRouterを使用する
    index.htmlと404.htmlを設置

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import EventList from "./pages/EventList";
 import EventDetail from "./pages/EventDetail";
@@ -7,20 +7,17 @@ import EventConfirm from "./pages/EventConfirm";
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route index element={<Navigate to="/events" />} />
-        {/* ネストしたルート */}
         <Route path="events" element={<Layout />}>
           <Route index element={<EventList />} />
-          {/* 動的な値に応じたURL */}
           <Route path=":id" element={<EventDetail />} />
           <Route path=":id/apply" element={<EventApply />} />
           <Route path=":id/confirm" element={<EventConfirm />} />
         </Route>
-        {/* 存在しないURLの処理 */}
         <Route path="*" element={<p>404 not found</p>} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
