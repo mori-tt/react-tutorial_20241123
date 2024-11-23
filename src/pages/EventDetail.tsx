541682c8208b8ab5003fb5c9b8290bb2627e4dd3
T_Mori <morit0301@gmail.com>
2024-11-23 18:00:34 +0900

    使ってないimport文削除

--- src/pages/EventDetail.tsx
+++ src/pages/EventDetail.tsx
@@ -1,4 +1,4 @@
-import { useParams, Link, useNavigate } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { EVENTS as events } from "../constants";
 import REACT_EVENT from "../assets/react-event.webp";
 import { CalendarDays, MapPin, Users } from "lucide-react";
